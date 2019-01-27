38dcfdc207594783b877093ecaf4271071673249
Fillan Lee <fillanlee@aol.com>
2019-01-27 03:44:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction (name)
   
 end
   
-def introduction_with_language (name, Language)
-  puts  ("Hi, my name is #{name} and I am learning to program in #{Language}\n")
+def introduction_with_language (name, language)
+  puts  ("Hi, my name is #{name} and I am learning to program in #{language}\n")
 end
